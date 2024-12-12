4693665ca7b4c3a6b9f9a2d91bfa68b33c719a61
VINEETH KUMAR JAMPAL <jampala250897@gmail.com>
2024-12-11 21:49:38 -0600

    Final commit

---
+++ src/components/about/Mission.js
@@ -0,0 +1,3 @@
+const Mission = () => <h2>Our mission is to build an application which show our technical expertise and understanding of the concepts.</h2>;
+
+export default Mission;
---
+++ src/components/about/Team.js
@@ -0,0 +1,3 @@
+const Team = () => <h1>This app was developed as a Final Project by our team!</h1>;
+
+export default Team;
